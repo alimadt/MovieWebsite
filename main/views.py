0912c16ea21c5f58ca6e82e601f14a1b0963ebda
alimadt <alima280300@gmail.com>
2021-11-27 19:43:55 +0600

    search

--- main/views.py
+++ main/views.py
@@ -1,3 +1,4 @@
+from django.db.models import Q
 from django.shortcuts import render
 from rest_framework import generics, viewsets, status
 from rest_framework.decorators import api_view, action
@@ -16,6 +17,14 @@ class GenreListView(generics.ListAPIView):
 class MovieViewSet(viewsets.ModelViewSet):
     queryset = Movie.objects.all()
     serializer_class = MovieSerializer
+
+    @action(detail=False, methods=['get'])
+    def search(self, request, pk=None):
+        q = request.query_params.get('q')
+        queryset = self.get_queryset()
+        queryset = queryset.filter(Q(title__icontains=q) | Q(description__icontains=q))
+        serializer = MovieSerializer(queryset, many=True, context={'request': request})
+        return Response(serializer.data, status=status.HTTP_200_OK)
 
     # def get_serializer_context(self):
     #     return {'request': self.request}
