0106f44a5c294044020d948ce84acb7b66fa2824
alimadt <alima280300@gmail.com>
2021-11-28 22:41:17 +0600

    swagger

--- movie/urls.py
+++ movie/urls.py
@@ -17,6 +17,9 @@ from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
+from drf_yasg import openapi
+from drf_yasg.views import get_schema_view
+from rest_framework import permissions
 from rest_framework.routers import DefaultRouter
 
 from main.views import GenreListView, MovieViewSet, MovieImageView, CommentViewSet, ChangePasswordView, RatingViewSet
@@ -26,8 +29,22 @@ router.register('movies', MovieViewSet)
 router.register('comments', CommentViewSet)
 router.register('rating', RatingViewSet)
 
+schema_view = get_schema_view(
+   openapi.Info(
+      title="StackOverFlow14 API",
+      default_version='v1',
+      description="Test description",
+      terms_of_service="https://www.google.com/policies/terms/",
+      contact=openapi.Contact(email="contact@snippets.local"),
+      license=openapi.License(name="BSD License"),
+   ),
+   public=True,
+   permission_classes=(permissions.AllowAny,),
+)
+
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', schema_view.with_ui()),
     path('v1/api/genres/', GenreListView.as_view()),
     path('v1/api/', include(router.urls)),
     path('v1/api/add-image/', MovieImageView.as_view()),
